1dacd93b2d0b3d923b7088f04a7e6542a5e1625b
Victor Moura <victorttmoura@gmail.com>
2024-04-28 23:44:17 -0300

    chore: add husky and lint staged setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+const name = "";
+console.log(name);
+
 const Home = () => {
-  return <div className="bg-red-500 py-5 mx-6"></div>
+  return <div className="mx-6 bg-red-500 py-5"></div>;
 };
 
 export default Home;
